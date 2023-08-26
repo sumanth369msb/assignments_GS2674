b7ba36111bec297e69eaee8bfed0848689c98235
sumanth369msb <72481925+sumanth369msb@users.noreply.github.com>
2023-08-26 18:21:46 +0530

    Update and rename Assignment: using list, dict to Assignment: using list, dict.py

--- Python Assignments/Assignment: using list, dict.py
+++ Python Assignments/Assignment: using list, dict.py
@@ -1,3 +1,13 @@
+# Write a Python class Restaurant with attributes like menu_items, book_table, and 
+# customer_orders, and methods like add_item_to_menu, book_tables, and customer_order.
+# Perform the following tasks now:
+# Now add items to the menu.
+# Make table reservations.
+# Take customer orders.
+# Print the menu.
+# Print table reservations.
+# Print customer orders.
+
 class Restaurent:
     def __init__(self,menu_items, book_table, customer_orders):
         self.menu_items=menu_items
