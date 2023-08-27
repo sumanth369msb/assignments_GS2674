02044a72f21153c8849b5b855c3d1b316581f6ea
sumanth369msb <72481925+sumanth369msb@users.noreply.github.com>
2023-08-28 03:02:29 +0530

    Create Assignment-4.py

---
+++ Python Assignments/Assignment-4.py
@@ -0,0 +1,36 @@
+####1.Write a Python program that prompts the user to input two numbers and raises a TypeError exception if the inputs are not numerical.
+
+try:
+    num1 = int(input("Enter the 1st number: "))
+    num2 = int(input("Enter the 2nd number: "))
+
+    res = num1 + num2
+
+    print(res)
+
+except ValueError as e:
+    print("Not a valid number.")
+
+
+######2. Write a Python program that prompts the user to input a number and handles a KeyboardInterrupt exception if the user cancels the input.
+
+try:
+    n = float(input("Enter a number)
+    print(n)
+
+except KeyboardInterrupt:
+    print("\nInput canceled by the user.")
+
+
+            
+#####4.Write a Python program that executes a list operation and handles an AttributeError exception if the attribute does not exist.
+
+try:
+    l1 = [11, 22, 33, 44, 55]
+    length = a.length
+
+except AttributeError as e:
+    print("AttributeError:", e)
+    
+l1.insert(3,66)
+print(l1)
